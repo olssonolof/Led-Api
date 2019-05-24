84bea94d6f5dd5f34a62ff7e6e7fd00a19ee5ca3
Olof <olssonolof@hotmail.com>
2019-05-24 11:57:19 +0200

    Initial commit

---
+++ Led-Api/Controllers/LedController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Device.Gpio;
+
+
+namespace Led_Api.Controllers
+{
+    [Route("/[controller]")]
+    [ApiController]
+    public class LedController : ControllerBase
+    {
+        // GET: /Led/blink
+        [HttpGet("blink")]
+        public async Task Blink()
+        {
+
+            int yellow = 21;
+            using (GpioController controller = new GpioController())
+            {
+                controller.OpenPin(yellow, PinMode.Output);
+                controller.Write(yellow, PinValue.High);
+                await Task.Delay(3000);
+                controller.Write(yellow, PinValue.Low);
+
+            }
+
+
+
+        }
+
+        // GET: /Led/on
+        [HttpGet("on")]
+        public void LedOn()
+        {
+            int yellow = 21;
+            GpioController controller = new GpioController();
+            
+                controller.OpenPin(yellow, PinMode.Output);
+                controller.Write(yellow, PinValue.High);
+
+            
+        }
+
+        // POST: api/off
+        [HttpGet("off")]
+        public void LedOff()
+        {
+
+            int yellow = 21;
+            GpioController controller = new GpioController();
+            
+                controller.OpenPin(yellow, PinMode.Output);
+              
+                controller.Write(yellow, PinValue.Low);
+
+            
+
+
+        }
+
+    }
+}
